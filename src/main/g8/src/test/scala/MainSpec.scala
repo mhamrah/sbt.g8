29f1917ec026ca18974f5954f5007dfd0975c813
mhamrah <mhamrah@gmail.com>
2013-04-25 09:49:54 -0500

    fixing formating issue; should have tested

--- src/main/g8/src/test/scala/MainSpec.scala
+++ src/main/g8/src/test/scala/MainSpec.scala
@@ -2,7 +2,7 @@ package $project_group_id$.$name;format="norm,word"$
 import org.scalatest.FunSpec
 
 class MainSpec extends FunSpec {
-  describe("$name") {
+  describe("$name;format="norm,word"$") {
     it("can pass a test") {
       pending
     }
