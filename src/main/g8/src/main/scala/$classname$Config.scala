539f8cda7a7cfddceb72573c62f5f0666c3b944f
mhamrah <mhamrah@gettyimages.com>
2013-08-23 11:09:03 -0400

    Adding config object pattern

---
+++ src/main/g8/src/main/scala/$classname$Config.scala
@@ -0,0 +1,9 @@
+package $project_group_id$.$name;format="norm,word"$
+
+import com.typesafe.config.ConfigFactory
+
+object $name;format="Camel"$Config {
+  private val config = ConfigFactory.load()
+
+  //Config Settings
+}
