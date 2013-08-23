852ca1f13a6969221dfe8cb237a611be00e7eca5
mhamrah <mhamrah@gettyimages.com>
2013-08-23 11:40:36 -0400

    moving spec file

--- src/main/g8/src/test/scala/$name_Camel$Spec.scala
+++ src/main/g8/src/test/scala/$name_Camel$Spec.scala
@@ -2,7 +2,7 @@ package $project_group_id$.$name;format="norm,word"$
 import org.scalatest._
 import org.scalatest.matchers._
 
-class MainSpec extends FreeSpec with ShouldMatchers {
+class MainSpec extends FreeSpec with Matchers {
   "The $name;format="norm,word"$ class" - {
     "should be able to run" in {
       pending
