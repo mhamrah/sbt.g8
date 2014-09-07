9a519d0b58dc08e732ec2396c86c936d5065855a
Michael Hamrah <m@hamrah.com>
2014-09-07 17:32:18 -0400

    fixing spec name

--- src/main/g8/src/test/scala/$name__Camel$Spec.scala
+++ src/main/g8/src/test/scala/$name__Camel$Spec.scala
@@ -2,7 +2,7 @@ package $project_group_id$.$name;format="norm,word"$
 import org.scalatest._
 import org.scalatest.matchers._
 
-class MainSpec extends FreeSpec with Matchers {
+class $name;format="Camel"$Spec extends FreeSpec with Matchers {
   "The $name;format="norm,word"$ class" - {
     "should be able to run" in {
       pending
