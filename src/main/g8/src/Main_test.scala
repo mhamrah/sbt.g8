36640f676478041687eaf271c69f4e447073a96a
Michael Hamrah <m@hamrah.com>
2015-02-25 15:13:20 -0500

    formatting

--- src/main/g8/src/Main_test.scala
+++ src/main/g8/src/Main_test.scala
@@ -2,8 +2,10 @@ package $project_group_id$.$name;format="norm,word"$
 import org.scalatest._
 import org.scalatest.matchers._
 
-class MainSpec extends FreeSpec with Matchers {
-  "The $name;format="norm,word"$ class" - {
+class MainSpec
+  extends FreeSpec
+  with Matchers {
+  "The Main App" - {
     "should be able to run" in {
       pending
     }
